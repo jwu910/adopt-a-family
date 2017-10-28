93e0c115af9aab399d6fb056073bb057222d73f8
Duc Ngo <duc.ngos.it.all@gmail.com>
2017-10-27 23:22:27 -0700

    AAF-44 empty out everything in Nav.js and move all the links in the header into the Nav components. Put the nav component into the header component. Update the snapshot for Nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Nav from './Nav';
 
 const Header = (props) => {
   return (
     <header>
-      <div className='header-text'>
-        <Link to='/'>
-          Adopt A Family
-        </Link>
-      </div>
-      <div className="header-links">
-        <div className="header-mission">
-          Mission
-        </div>
-        <div className="header-family">
-          Our Families
-        </div>
-        <div className="headet-about">
-          <Link to="/aboutus">
-            About us
-          </Link>
-        </div>
-        <div className="header-donate">
-          Donate
-        </div>
-        <Link to="/registration">
-          <button className="button-login">
-            ADOPT A FAMILY
-          </button>
-        </Link>
-      </div>
+      <Nav />
     </header>
   );
 };
