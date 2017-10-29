00c234044b1567953c4929c7384ac8d43ad76e77
Duc Ngo <duc.ngos.it.all@gmail.com>
2017-10-29 10:50:48 -0700

    remove an extraneous div element and update the snap

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,33 +6,31 @@ class Nav extends Component {
   render () {
     return (
       <div>
-        <div>
-          <div className='header-text'>
-            <Link to='/'>
-              Adopt A Family
+        <div className='header-text'>
+          <Link to='/'>
+            Adopt A Family
+          </Link>
+        </div>
+        <div className="header-links">
+          <div className="header-mission">
+            Mission
+          </div>
+          <div className="header-family">
+            Our Families
+          </div>
+          <div className="header-about">
+            <Link to="/aboutus">
+              About us
             </Link>
           </div>
-          <div className="header-links">
-            <div className="header-mission">
-              Mission
-            </div>
-            <div className="header-family">
-              Our Families
-            </div>
-            <div className="header-about">
-              <Link to="/aboutus">
-                About us
-              </Link>
-            </div>
-            <div className="header-donate">
-              Donate
-            </div>
-            <Link to="/registration">
-              <button className="button-login">
-                ADOPT A FAMILY
-              </button>
-            </Link>
+          <div className="header-donate">
+            Donate
           </div>
+          <Link to="/registration">
+            <button className="button-login">
+              ADOPT A FAMILY
+            </button>
+          </Link>
         </div>
       </div>
     );
