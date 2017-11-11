4f9e6f2f96fea924d12cb78cc075615c96d0bcf6
protoEvangelion <ryantgarant@gmail.com>
2017-11-10 16:47:41 -0800

    Added static express middleware

--- server/app.js
+++ server/app.js
@@ -1,16 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3005;
 const indexRoutes = require('./routes');
 const apiRoutes = require('./routes/api');
 
-// Eventually we will need to refer to the build folder for production
-// It would look like: 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
-
 app.use('/', indexRoutes);
 app.use('/api', apiRoutes);
+
+app.use('/5-gwoap/adopt-a-family/', express.static(
+  path.join(
+    process.cwd(),
+    process.env.NODE_ENV === "development" ? "public" : "build"
+  )
+));
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
